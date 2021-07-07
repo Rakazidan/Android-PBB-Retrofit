c87deb9e3bb9a8de53821026c1a23a98c1afe3f7
Raka Zidan <raka.zidan20@gmail.com>
2021-07-07 18:18:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/retrofitexample/Post.java
@@ -0,0 +1,37 @@
+package com.example.retrofitexample;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+
+    private int userID;
+
+    private Integer id;
+
+    private String title;
+
+    @SerializedName("body")
+    private String text;
+
+    public Post(int userID, String title, String text) {
+        this.userID = userID;
+        this.title = title;
+        this.text = text;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
